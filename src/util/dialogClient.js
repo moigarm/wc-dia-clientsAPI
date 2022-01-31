fb5643746933d76975e6431611245ae60c98b096
Ismael Pérez <moisesgarmendia@hotmail.com>
2022-01-30 21:52:12 -0600

    retorno de objeto paramters procedente de DialogFlow

--- src/util/dialogClient.js
+++ src/util/dialogClient.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
 
-
 function getParameters(Objeto){
     let paramObj = Objeto?.queryResult?.parameters
     let params = process.env.DIALOG_PARAMS.split(",")
-    
+    let newObj = {}
+    params.forEach((key)=>{
+        newObj[key] = paramObj[key]
+    })
+    return newObj
 }
 
 module.exports = [
