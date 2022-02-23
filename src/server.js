47f647f885bbc4a59743eea0db60fa72df38d577
CarlosRocha2409 <carlos.eduardo.rocha.silva@gmail.com>
2022-02-22 19:38:34 -0600

    Adding Docker files

--- src/server.js
+++ src/server.js
@@ -11,24 +11,24 @@ const swaggerUi = require("swagger-ui-express");
 const diaClientController = require("./controllers/diaClientController");
 const wooClientController = require("./controllers/wooClientController");
 const wooProductoController = require("./controllers/wooProductoController");
-const { convertToCronSyntax } = require("./util/timeConverter")
+const { convertToCronSyntax } = require("./util/timeConverter");
 
 //Endpoint for testing purposes
 const dialogFlowController = require("./controllers/dialogFlowController");
 
 var cron = require("node-cron");
 
-const { cronSchedule } = require("./cronSchedule")
+const { cronSchedule } = require("./cronSchedule");
 
 let allowedOrigins = [`http://localhost:${process.env.PORT}`];
-let secondsFromEnv = process.env.CRON_SECONDS
-const cronValue = convertToCronSyntax(secondsFromEnv)
+let secondsFromEnv = process.env.CRON_SECONDS;
+const cronValue = convertToCronSyntax(secondsFromEnv);
 
-// console.log("SECONDS: "+secondsFromEnv)
-// console.log("CRON VALUE IS: "+cronValue)
-// cron.schedule(cronValue, async() =>{
-//   await cronSchedule()
-// });
+console.log("SECONDS: " + secondsFromEnv);
+console.log("CRON VALUE IS: " + cronValue);
+cron.schedule(cronValue, async () => {
+  await cronSchedule();
+});
 
 var app = express();
 app.use(
@@ -54,22 +54,22 @@ app.use(
 );
 app.use(bodyparser.json());
 
-
 const swaggerOptions = {
   swaggerDefinition: {
     info: {
       version: "1.0.0",
       title: "Colechera a WooCommerce",
-      description: "API encargado de reflejar los datos obtenidos de Biman a WooCommerce,"+
-      " datos de clientes potenciales de DialogFlow a SuiteCRM, datos de clientes al ordenar "+
-      "un producto en WooCommerce a suiteCRM",
+      description:
+        "API encargado de reflejar los datos obtenidos de Biman a WooCommerce," +
+        " datos de clientes potenciales de DialogFlow a SuiteCRM, datos de clientes al ordenar " +
+        "un producto en WooCommerce a suiteCRM",
       contact: {
-        name: "HayTic"
+        name: "HayTic",
       },
-      servers: [`http://localhost:${process.env.PORT}`]
-    }
+      servers: [`http://localhost:${process.env.PORT}`],
+    },
   },
-  apis: ["src/controllers/*.js"]
+  apis: ["src/controllers/*.js"],
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
@@ -78,7 +78,6 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.listen(process.env.PORT, () => {
   console.log(`Servidor Express inció en puerto : ${process.env.PORT}`);
 });
-
 
 app.use("/diaClient", diaClientController);
 app.use("/wooClient", wooClientController);
